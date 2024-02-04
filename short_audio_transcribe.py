de8441d698cbfed043b6f92b4a3c9217d8655caa
Zedtion <64766171+Zedtion@users.noreply.github.com>
2024-02-04 13:37:41 +0800

    Create short_audio_transcribe.py

---
+++ short_audio_transcribe.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+import traceback
+import os
+import sys
+from modelscope.pipelines import pipeline
+from modelscope.utils.constant import Tasks
+
+dataset_name = 'ada'
+dir = './Data/ada/wavs'
+# opt_name=dir.split("\\")[-1].split("/")[-1]
+opt_name = os.path.basename(dir)
+inference_pipeline = pipeline(
+    task=Tasks.auto_speech_recognition,
+    model='./damo_models/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch',
+    vad_model='./damo_models/speech_fsmn_vad_zh-cn-16k-common-pytorch',
+    punc_model='./damo_models/punc_ct-transformer_zh-cn-common-vocab272727-pytorch',
+)
+
+opt = []
+for name in os.listdir(dir):
+    print(name)
+    try:
+        text = inference_pipeline(input="%s/%s" % (dir, name))[0]['text']
+        opt.append("%s/%s|%s|ZH|%s" % (dir, name, opt_name, text))
+    except:
+        print(traceback.format_exc())
+
+opt_dir = "./damo_opt"
+os.makedirs(opt_dir, exist_ok=True)
+with open(f"{opt_dir}/{dataset_name}.list", "w", encoding="utf-8")as f:
+    f.write("\n".join(opt))
